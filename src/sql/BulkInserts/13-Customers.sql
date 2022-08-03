7740f2af23dbe771cbe03fcfa8d7e847b049a771
NicKohSE3 <nicholaskse03.20@ichat.sp.edu.sg>
2022-08-03 12:17:33 +0800

    Create 13-Customers.sql

---
+++ src/sql/BulkInserts/13-Customers.sql
@@ -0,0 +1,9 @@
+BULK INSERT sales.customers
+FROM 'C:\SP\Notes, Practicals etc\AY 2223\DENG\Assignment2\src\csv\customers.csv' -- Replace with your own path
+WITH
+(
+    FIRSTROW = 2,
+    FIELDTERMINATOR = ',',
+    ROWTERMINATOR = '\n',
+    TABLOCK
+)
