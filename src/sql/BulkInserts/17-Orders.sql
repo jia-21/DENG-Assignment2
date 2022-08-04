31464cab36db2c3d53926fb68eb5c938fe6bd29f
NicKohSE3 <nicholaskse03.20@ichat.sp.edu.sg>
2022-08-04 21:32:28 +0800

    how to insert orders.csv

--- src/sql/BulkInserts/17-Orders.sql
+++ src/sql/BulkInserts/17-Orders.sql
@@ -6,3 +6,17 @@ WITH
     FIELDTERMINATOR = ',',
     ROWTERMINATOR = '\n'
 )
+
+/*
+    If that does not work, import the CSV file manually
+    Use this as a guide
+    https://qawithexperts.com/article/sql/import-csv-into-sql-server-with-query-or-without-query-using/265
+    Afterwards, import all the data from your 2nd table with this query
+    (You can't import to an existing table for some reason)
+    INSERT INTO
+    <1st table>
+    SELECT
+    *
+    FROM
+    <2nd table>;
+*/
