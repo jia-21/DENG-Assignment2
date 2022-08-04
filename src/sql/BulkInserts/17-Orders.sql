d12dcaef9c8091d3475f313e1fd7b0cb97d43377
NicKohSE3 <nicholaskse03.20@ichat.sp.edu.sg>
2022-08-04 21:41:53 +0800

    IMPORTANT

--- src/sql/BulkInserts/17-Orders.sql
+++ src/sql/BulkInserts/17-Orders.sql
@@ -12,6 +12,7 @@ WITH
     Use this as a guide
     https://qawithexperts.com/article/sql/import-csv-into-sql-server-with-query-or-without-query-using/265
     Afterwards, import all the data from your 2nd table with this query
+    ! IMPORTANT; YOU NEED TO MAKE SURE THE DATA TYPES ARE THE SAME AS THE sales.orders TABLE
     (You can't import to an existing table for some reason)
     INSERT INTO
     <1st table>
