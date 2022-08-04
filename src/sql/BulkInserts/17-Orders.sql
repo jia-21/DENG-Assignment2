8e6101862bac6775b6583b4907aa15f658435a0e
NicKohSE3 <nicholaskse03.20@ichat.sp.edu.sg>
2022-08-04 21:47:45 +0800

    TLDR + MORE IMPT

--- src/sql/BulkInserts/17-Orders.sql
+++ src/sql/BulkInserts/17-Orders.sql
@@ -12,7 +12,9 @@ WITH
     Use this as a guide
     https://qawithexperts.com/article/sql/import-csv-into-sql-server-with-query-or-without-query-using/265
     Afterwards, import all the data from your 2nd table with this query
+    TLDR; Right click on the Assignment2 database > Tasks > Import Flat File > Specify Input File > Browse... > Find CSV file >
     ! IMPORTANT; YOU NEED TO MAKE SURE THE DATA TYPES ARE THE SAME AS THE sales.orders TABLE
+    ! IMPORTANT; MAKE SURE THE SCHEMA IS sales
     (You can't import to an existing table for some reason)
     INSERT INTO
     <1st table>
