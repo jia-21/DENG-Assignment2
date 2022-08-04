b702e430af9e45d79ed943c884787e720e115db7
NicKohSE3 <nicholaskse03.20@ichat.sp.edu.sg>
2022-08-04 22:30:06 +0800

    USE [Assignment2];

--- src/sql/BulkInserts/14-Stores.sql
+++ src/sql/BulkInserts/14-Stores.sql
@@ -1,3 +1,5 @@
+USE [Assignment2];
+
 BULK INSERT sales.stores
 FROM 'C:\SP\Notes, Practicals etc\AY 2223\DENG\Assignment2\src\txt\store.txt' -- Replace this with your own path
 WITH
--- src/sql/BulkInserts/16-Staff.sql
+++ src/sql/BulkInserts/16-Staff.sql
@@ -1,3 +1,5 @@
+USE [Assignment2];
+
 BULK INSERT sales.staffs
 FROM 'C:\SP\Notes, Practicals etc\AY 2223\DENG\Assignment2\src\txt\staff.txt' -- Replace this with your own path
 WITH
