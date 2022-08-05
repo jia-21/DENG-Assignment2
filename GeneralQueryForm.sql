4dbd6e4faa0237d105113dcf2948ac40e577d3c2
NicKohSE3 <nicholaskse03.20@ichat.sp.edu.sg>
2022-08-05 09:52:17 +0800

    Create GeneralQueryForm.sql

---
+++ GeneralQueryForm.sql
@@ -0,0 +1,14 @@
+SELECT
+    -- Column list
+FROM
+    -- Table list
+JOIN
+    -- Join list
+WHERE
+    -- Condition list
+GROUP BY
+    -- Column grouping
+HAVING
+    -- Condition
+ORDER BY
+    -- Column name(s)
