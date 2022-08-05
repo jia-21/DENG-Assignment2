8ec9cf324dda58c3800e9f442762c83149197c63
MattLJH <103894261+MattLJH@users.noreply.github.com>
2022-08-05 16:11:42 +0800

    q2 q5

--- sql_codes/3-Queries/Query2.sql
+++ sql_codes/3-Queries/Query2.sql
@@ -1,1 +1,14 @@
 -- Query 2 - Sales/Staff/stores
+select
+	StaffKey,
+	StoreKey,
+	sum(ListPrice) as 'Amt Earned'
+from
+	SalesFacts
+Group by
+	StaffKey,
+	StoreKey
+Order by
+	'Amt Earned' asc
+
+-- Amt earned by each staff in each store
--- sql_codes/3-Queries/Query5.sql
+++ sql_codes/3-Queries/Query5.sql
@@ -1,1 +1,25 @@
 -- Query 5 - Sales/Products/brands/categories/inventory
+Select
+	sf.productKey,
+	count(sf.ProductKey) as 'Total Sold',
+	b.BrandName,
+	c.CategoryName,
+	sum(sf.ListPrice) as 'Amt Earned'
+from
+	ProductDim p
+left join
+	SalesFacts sf on sf.ProductKey = p.ProductKey
+right join
+	BrandDIM b ON b.BrandKey = p.BrandId
+left join
+	CategoryDIM c ON c.CategoryKey = p.CategoryId
+where
+	sf.productKey is not null
+group by
+	sf.ProductKey,
+	b.BrandName,
+	c.CategoryName
+order by
+	BrandName desc,
+	CategoryName desc,
+	[Amt Earned] asc
